9ac6de8cd894d18e5854c0b91a1c97a2aca3b17f
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-09-16 09:40:40 +0530

    notification for long distance journeys

--- vms-master/resources/views/journey/requests.blade.php
+++ vms-master/resources/views/journey/requests.blade.php
@@ -13,12 +13,155 @@
 @section('content')
     @include('layouts.errors')
     @include('layouts.success')
+
+    @if(Auth::user()->name =="Director Name" ) 
+    {{-- <p>{{Auth::user()?Auth::user()->name:''}}</p> --}}
+        @if($longDisJourneys)
+            <div class="alert alert-info alert-dismissible fade in">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Need Attention for Long Distance Journeys Request List...</strong> 
+            </div>
+        @endif
+
+            {{-- for long distance journeys --}}
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h2 class="box-title"> <strong> Long Distance Journey Request List </strong> </h2>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+    
+            <div class="box-body">
+                @if($longDisJourneys)
+                    <table class="table" id="longDistable" style="height:150px; overflow: auto;">
+                        <thead>
+                        <tr>
+                            <th>Applicant Name</th>
+                            <th>Applicant Division</th>
+                            <th>Vehicle</th>
+                            <th>Start Date / Time</th>
+                            <th>End Date / Time</th>
+                            <th width="150px">Expected Distance</th>
+                            <th>Updated at</th>
+                            <th width="200px">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($longDisJourneys as $journey)
+                            <tr>
+                                <td>{{$journey->applicant->emp_surname}}</td>
+                                <td>{{$journey->applicant->division->dept_name}}</td>
+                                <td>{{$journey->vehical->fullname}}</td>
+                                <td>{{$journey->expected_start_date_time->toDayDateTimeString()}}</td>
+                                <td>{{$journey->expected_end_date_time->toDayDateTimeString()}}</td>
+                                <th> {{ $journey->expected_distance }} </th> 
+                                <td>{{$journey->applicant->emp_surname}}</td>
+    
+                                <td width="200px">
+                                    <button id="{{$journey->id}}" class="btn btn-success btnVie"><i class="fa fa-eye"></i></button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>Applicant Name</th>
+                            <th>Applicant Division</th>
+                            <th>Vehicle</th>
+                            <th>Start Date / Time</th>
+                            <th>End Date / Time</th>
+                            <th>Updated at</th>
+                            <th width="200px">Actions</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                    @foreach($longDisJourneys as $journey)
+                        <div id="d{{$journey->id}}" hidden="hidden">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Journey Request Approval <span class="label label-danger pull-right">Not Approved</span></h3>
+                                    <hr>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <dl class="dl-horizontal">
+                                        <h4>Applicant</h4>
+                                        <dt>Name</dt>
+                                        <dd>{{$journey->applicant->emp_surname}}</dd>
+                                        <dt>Division</dt>
+                                        <dd>{{$journey->applicant->division->dept_name}}</dd>
+                                        <dt>Email</dt>
+                                        <dd>{{$journey->applicant->emp_email}}</dd>
+                                    </dl>
+                                    <dl class="dl-horizontal">
+                                        <h4>Resources</h4>
+                                        <dt>Vehicle Number</dt>
+                                        <dd>{{$journey->vehical->registration_no}}</dd>
+                                        <dt>Vehicle Name</dt>
+                                        <dd>{{$journey->vehical->name}}</dd>
+                                        <dt>Driver</dt>
+                                        <dd>{{$journey->vehical->driver->fullname}}</dd>
+                                    </dl>
+                                    <dl class="dl-horizontal">
+                                        <dt>Divisional Head</dt>
+                                        <dd>{{$journey->divisional_head->emp_title.' '.$journey->divisional_head->emp_initials.'. '.$journey->divisional_head->emp_surname}}</dd>
+                                    </dl>
+                                </div>
+                                <div class="col-md-6">
+                                    <dl class="dl-horizontal">
+                                        <h4>Details</h4>
+                                        <dt>Purpose</dt>
+                                        <dd>{{$journey->purpose}}</dd>
+                                        <dt>Places To Be Visited</dt>
+                                        <dd>{{$journey->places_to_be_visited}}</dd>
+                                        <dt>Number of Persons</dt>
+                                        <dd>{{$journey->number_of_persons}}</dd>
+                                        <dt>Approximate Distance</dt>
+                                        <dd>{{$journey->expected_distance.' km'}}</dd>
+                                    </dl>
+                                    <dl class="dl-horizontal">
+                                        <h4>Expected Date and Time Range</h4>
+                                        <dt>Start Date/ Time</dt>
+                                        <dd>{{$journey->expected_start_date_time->toDayDateTimeString()}}</dd>
+                                        <dt>End Date/ Time</dt>
+                                        <dd>{{$journey->expected_end_date_time->toDayDateTimeString()}}</dd>
+                                    </dl>
+    
+                                </div>
+                            </div>
+                            <div class="row">
+                                <hr>
+                                <div class="col-md-12"> 
+                                    {!! Form::open(['method' => 'post','id'=>'formApprovalLong','action'=>['JourneyRequestController@approval',$journey->id]]) !!}
+                                    <div class="form-group">
+                                        <label for="remarks">Remarks</label>
+                                        {{ Form::textarea('remarks', null, ['class'=>'form-control','placeholder'=>'Remarks','rows'=>'2' ]) }}
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="is_approved">Approve</label> 
+                                        {!! Form::checkbox('is_approved', 1, true) !!}
+                                    </div>
+                                    <div> 
+                                        {{Form::submit('SUBMIT', ['class'=>'btn btn-info pull-left'])}}
+                                    </div>
+    
+                                    {!! Form::close() !!}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif   
+            </div>
+        </div>
+    @endif
+
+        {{-- for journeys that did not request long distance --}}
     <div class="box box-primary">
+
         <div class="box-header with-border">
             <h3 class="box-title">Journey Requests List</h3>
-        </div>
-        <!-- /.box-header -->
-        <!-- form start -->
+        </div> 
 
         <div class="box-body">
             @if($journeys)
@@ -123,11 +266,11 @@
                                 {!! Form::open(['method' => 'post','id'=>'formApproval','action'=>['JourneyRequestController@approval',$journey->id]]) !!}
                                 <div class="form-group">
                                     <label for="remarks">Remarks</label>
-                                    {{Form::textarea('remarks',null,['class'=>'form-control','placeholder'=>'Remarks','rows'=>'2' ])}}
+                                    {!!Form::textarea('remarks',null,['class'=>'form-control','placeholder'=>'Remarks','rows'=>'2' ]) !!}
                                 </div>
                                 <div class="form-group">
                                     <label for="is_approved">Approve</label> &nbsp
-                                    {!! Form::checkbox('is_approved', 1, true); !!}
+                                    {!! Form::checkbox('is_approved', 1, true) !!}
                                 </div>
                                 {!! Form::close() !!}
                             </div>
@@ -138,135 +281,6 @@
 
         </div>
     </div>
-
-    {{-- for long distance journeys --}}
-
-    <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">Long Distance Journey Requests List</h3>
-            </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-    
-            <div class="box-body">
-                @if($longDisJourneys)
-                    <table class="table" id="longDistable" style="height:30px; overflow: auto;">
-                        <thead>
-                        <tr>
-                            <th>Applicant Name</th>
-                            <th>Applicant Division</th>
-                            <th>Vehicle</th>
-                            <th>Start Date / Time</th>
-                            <th>End Date / Time</th>
-                            <th>Updated at</th>
-                            <th width="200px">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($longDisJourneys as $journey)
-                            <tr>
-                                <td>{{$journey->applicant->emp_surname}}</td>
-                                <td>{{$journey->applicant->division->dept_name}}</td>
-                                <td>{{$journey->vehical->fullname}}</td>
-                                <td>{{$journey->expected_start_date_time->toDayDateTimeString()}}</td>
-                                <td>{{$journey->expected_end_date_time->toDayDateTimeString()}}</td>
-                                <td>{{$journey->applicant->emp_surname}}</td>
-    
-                                <td width="200px">
-                                    <button id="{{$journey->id}}" class="btn btn-success btnVie"><i class="fa fa-eye"></i></button>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <th>Applicant Name</th>
-                            <th>Applicant Division</th>
-                            <th>Vehicle</th>
-                            <th>Start Date / Time</th>
-                            <th>End Date / Time</th>
-                            <th>Updated at</th>
-                            <th width="200px">Actions</th>
-                        </tr>
-                        </tfoot>
-                    </table>
-                    @foreach($longDisJourneys as $journey)
-                        <div id="d{{$journey->id}}" hidden="hidden">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h3>Journey Request Approval <span class="label label-danger pull-right">Not Approved</span></h3>
-                                    <hr>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <dl class="dl-horizontal">
-                                        <h4>Applicant</h4>
-                                        <dt>Name</dt>
-                                        <dd>{{$journey->applicant->emp_surname}}</dd>
-                                        <dt>Division</dt>
-                                        <dd>{{$journey->applicant->division->dept_name}}</dd>
-                                        <dt>Email</dt>
-                                        <dd>{{$journey->applicant->emp_email}}</dd>
-                                    </dl>
-                                    <dl class="dl-horizontal">
-                                        <h4>Resources</h4>
-                                        <dt>Vehicle Number</dt>
-                                        <dd>{{$journey->vehical->registration_no}}</dd>
-                                        <dt>Vehicle Name</dt>
-                                        <dd>{{$journey->vehical->name}}</dd>
-                                        <dt>Driver</dt>
-                                        <dd>{{$journey->vehical->driver->fullname}}</dd>
-                                    </dl>
-                                    <dl class="dl-horizontal">
-                                        <dt>Divisional Head</dt>
-                                        <dd>{{$journey->divisional_head->emp_title.' '.$journey->divisional_head->emp_initials.'. '.$journey->divisional_head->emp_surname}}</dd>
-                                    </dl>
-                                </div>
-                                <div class="col-md-6">
-                                    <dl class="dl-horizontal">
-                                        <h4>Details</h4>
-                                        <dt>Purpose</dt>
-                                        <dd>{{$journey->purpose}}</dd>
-                                        <dt>Places To Be Visited</dt>
-                                        <dd>{{$journey->places_to_be_visited}}</dd>
-                                        <dt>Number of Persons</dt>
-                                        <dd>{{$journey->number_of_persons}}</dd>
-                                        <dt>Approximate Distance</dt>
-                                        <dd>{{$journey->expected_distance.' km'}}</dd>
-                                    </dl>
-                                    <dl class="dl-horizontal">
-                                        <h4>Expected Date and Time Range</h4>
-                                        <dt>Start Date/ Time</dt>
-                                        <dd>{{$journey->expected_start_date_time->toDayDateTimeString()}}</dd>
-                                        <dt>End Date/ Time</dt>
-                                        <dd>{{$journey->expected_end_date_time->toDayDateTimeString()}}</dd>
-                                    </dl>
-    
-                                </div>
-                            </div>
-                            <div class="row">
-                                <hr>
-                                <div class="col-md-12">
-                                    {!! Form::open(['method' => 'post','id'=>'formApprovalLong','action'=>['JourneyRequestController@approval',$journey->id]]) !!}
-                                    <div class="form-group">
-                                        <label for="remarks">Remarks</label>
-                                        {{Form::textarea('remarks',null,['class'=>'form-control','placeholder'=>'Remarks','rows'=>'2' ])}}
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="is_approved">Approve</label> &nbsp
-                                        {!! Form::checkbox('is_approved', 1, true); !!}
-                                    </div>
-                                    {!! Form::close() !!}
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                @endif
-    
-            </div>
-        </div>
-  
 
 @endsection
 
