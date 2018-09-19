c36668bef04703f34d2d037b96ca6f653e89bce7
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-09-19 23:21:35 +0530

    updated journeyConfirmationController

--- vms-master/app/Http/Controllers/JourneyConfirmController.php
+++ vms-master/app/Http/Controllers/JourneyConfirmController.php
@@ -61,11 +61,73 @@ class JourneyConfirmController extends Controller
 
         if($request->ajax()){ 
 
-            $journey = Journey::find($request->id);
-            //$journey = Journey::all();
-            
-            return response($journey);
+            if($journey = Journey::find($request->id)){
 
+                //$journey->confirmation_remarks = $request->remarks;
+                $journey->confirmed_at = Carbon::now();
+                $journey->confirmed_by = '000004';
+                $journey->confirmation_remarks = $request->confirmation_remarks;
+
+                // if($request->is_confirm == '1'){
+                //     return "sgsg";//response();
+                // }
+                
+                if($request->is_confirm == '1'){
+                    
+                    $journey->journey_status_id = '4';
+                    
+                    if($journey->driver_id!=$request->driver_id){
+                        $journey->driver_id = $request->driver_id;
+                    }
+
+                    if($journey->vehical_id!=$request->vehical_id){
+                        $journey->vehical_id = $request->vehical_id;
+                    }
+
+                    if($journey->vehical_id!=$request->vehical_id){
+                        $journey->vehical_id = $request->vehical_id;
+                    }
+
+                    if($journey->expected_start_date_time != Carbon::parse($request->confirmed_start_date_time)){
+                        $journey->confirmed_start_date_time = Carbon::parse($request->confirmed_start_date_time);
+                    }
+
+                    if($journey->expected_end_date_time != Carbon::parse($request->confirmed_end_date_time)){
+                        $journey->confirmed_end_date_time = Carbon::parse($request->confirmed_end_date_time);
+                    } 
+
+                    //$journey->update(); 
+                    // $redirect_url = route('/journey/requests/notconfirmed');
+
+                    // $report = ['url' => $redirect_url];
+                   
+                    // return response()->json($report);
+                    return response()->json([
+                        'success' => true,
+                        'message' => 'record updated'
+                    ], 200);
+                    //return response()->json(['success'=>'Journey request confirmed successfully !','result'=>$journey,'url'=> route('/journey/requests/notconfirmed')]);
+                    //return redirect()->back()->with(['success'=>'Journey request confirmed successfully !' , 'url'=> route('/journey/requests/notconfirmed')]);
+
+                }
+               
+                else{
+
+                    $journey->journey_status_id = '5';
+                    //$journey->update();
+                    //return response($journey);
+                    return redirect()->back()->with(['success'=>'Journey request confirmation denied successfully !']);
+
+                }
+
+            }
+            
+            //return response($journey);
         }
+
+    }
+
+    public function test(Request $request){
+        return $request;
     }
 }
