5e10624379dfac6be7e3a1002e8420ce2530504f
yasas1 <ranawaka.y@gmail.com>
2018-11-01 09:16:54 +0530

    updated4 mail

--- vms-master/resources/views/journey/createBacklogJourney.blade.php
+++ vms-master/resources/views/journey/createBacklogJourney.blade.php
@@ -65,12 +65,7 @@
                             <h4><i class="fa fa-user"></i> Applicant</h4>
                             <div class="col-md-offset-1">
                                 <div class="row">
-                                    {{-- <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="name"><h4>Name - {{Auth::user()->name}}</h4></label>
-                                        </div>
-                                    </div> --}}
-                                   
+                    
                                     <div class="form-group">
                                     {!! Form::hidden('applicant_id', null,['id'=>'textApplicant']); !!}
                                         <div class="btn-group dropdown col-md-12">
