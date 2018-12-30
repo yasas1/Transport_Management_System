51337a90a75a7518487a5d2d2be7ac0fa52abc34
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-12-30 21:31:37 +0530

    updated

---
+++ vms-master/resources/views/Mail/mailJourneyRequest.blade.php
@@ -0,0 +1,133 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        .btn {
+            background: #3bd43e;
+            background-image: -webkit-linear-gradient(top, #3bd43e, #42cc4d);
+            background-image: -moz-linear-gradient(top, #3bd43e, #42cc4d);
+            background-image: -ms-linear-gradient(top, #3bd43e, #42cc4d);
+            background-image: -o-linear-gradient(top, #3bd43e, #42cc4d);
+            background-image: linear-gradient(to bottom, #3bd43e, #42cc4d);
+            -webkit-border-radius: 6;
+            -moz-border-radius: 6;
+            border-radius: 6px;
+            font-family: Courier New;
+            color: #ffffff;
+            font-size: 23px;
+            padding: 10px 20px 10px 20px;
+            text-decoration: none;
+        }
+
+        .btn:hover {
+            background: #3cb0fd;
+            background-image: -webkit-linear-gradient(top, #3cb0fd, #3498db);
+            background-image: -moz-linear-gradient(top, #3cb0fd, #3498db);
+            background-image: -ms-linear-gradient(top, #3cb0fd, #3498db);
+            background-image: -o-linear-gradient(top, #3cb0fd, #3498db);
+            background-image: linear-gradient(to bottom, #3cb0fd, #3498db);
+            text-decoration: none;
+        }
+    </style>
+
+</head>
+<body>
+    <table style="border-collapse:collapse;margin:0 auto;max-width:600px;padding:0;width:100%">
+        <tbody>
+        <tr>
+            <td style="border-collapse:collapse;color:#e9e9e9;font-size:0;height:0;padding:0;width:100%" bgcolor="#e9e9e9">
+            </td>
+        </tr>
+        <tr>
+            <td style="border-collapse:collapse;color:#fff;padding:0;width:100%">
+                <table style="border-collapse:collapse;padding:0;width:100%" bgcolor="#3b0063">
+                    <tbody>
+                    <tr>
+                        <td style="border-collapse:collapse;padding:4%;text-align:center;vertical-align:middle;width:19.5%">
+                            <img src="http://ucsc.cmb.ac.lk/wp-content/themes/ucsc_theme/images/logo.png" style="display:block;width:100%" class="CToWUd">
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td style="border-collapse:collapse;padding:0;width:100%" bgcolor="#f7f7f7">
+                <table style="border-collapse:collapse;padding:0;width:100%">
+                    <tbody>
+                    <tr>
+                        <td style="border-collapse:collapse;padding:20px 0 20px 4%;width:40%">
+                            <h2>Vehicle Management System</h2>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td class="m_-7480336755895581727layout__cell m_-7480336755895581727layout__cell--content" style="border-bottom:1px solid #e9e9e9;border-collapse:collapse;border-top:1px solid #e9e9e9;padding:8%;width:100%" bgcolor="#f7f7f7">
+                <table class="m_-7480336755895581727content" style="border-collapse:collapse;padding:0;text-align:center;width:100%">
+                    <tbody class="m_-7480336755895581727content__body">
+                    <tr class="m_-7480336755895581727content__row">
+                        <td class="m_-7480336755895581727content__text" style="border-collapse:collapse;padding:0">
+                            <p style="color:#666;font-size:14px;margin:0">Welcome!</p>
+                            <p style="color:#666;font-size:14px;margin:0"> 
+                                This is to inform that you have a Journey request to approve as a devisional head.
+                            </p>
+                            <p style="color:#000000;font-size:14px;margin:0"> 
+                                {{$msg}}
+                            </p>
+                            
+                        </td>
+                    </tr>
+                    <tr class="m_-7480336755895581727content__row">
+                        <td class="m_-7480336755895581727content__pin" style="border-collapse:collapse;padding:32px 0 16px">
+                            <table class="m_-7480336755895581727pin" style="border-collapse:collapse;display:inline-block;padding:0;vertical-align:bottom">
+                                <tbody class="m_-7480336755895581727pin__body">
+                                <tr class="m_-7480336755895581727pin__row">
+                                    <td class="m_-7480336755895581727pin__cell m_-7480336755895581727pin__cell--text" style="border-collapse:collapse;color:#000;font-size:18px;font-weight:700;letter-spacing:2px;min-width:160px;padding:0 16px;text-align:center">
+                                        <a class="btn" href="#">CLink</a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td style="border-collapse:collapse;color:#666;font-size:12px;padding:20px;text-align:center;width:100%" bgcolor="#f7f7f7">
+                Vehicle Management System @ UCSC
+            </td>
+        </tr>
+        <tr>
+            <td style="border-collapse:collapse;color:#fff;padding:0;width:100%">
+                <table style="border-collapse:collapse;padding:0;width:100%" bgcolor="#3b0063">
+                    <tbody>
+                    <tr>
+                        <td style="border-collapse:collapse;padding:0 4%;width:82.5%">
+                            <table style="border-collapse:collapse;padding:0">
+                                <tbody>
+                                <tr>
+                                    <td style="border-collapse:collapse;padding:0"> </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td style="border-collapse:collapse;padding:24px 4%;width:17.5%"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</body>
+</html>
